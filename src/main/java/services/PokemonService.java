9b1ad710aa73a7436bf3eff29a2565857af2720c
hlin <Zxsaqw12@&$>
2020-03-24 17:22:48 -0400

    add entityManager

--- src/main/java/services/PokemonService.java
+++ src/main/java/services/PokemonService.java
@@ -1,11 +1,19 @@
 package services;
 
 
+import entities.Pokemon;
+
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 
 public class PokemonService {
 
-   // private static EntityManagerFactory emf = Persistence.createEntityManagerFactory()
+    private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("my-persistence-unit");
+    private static EntityManager em = emf.createEntityManager();
+
+    private static void persistPokemon(Pokemon pokemon) { em.persist(pokemon); }
+
+    private static Pokemon findPokemon(Integer id) { return em.find(Pokemon.class, id); }
 }
